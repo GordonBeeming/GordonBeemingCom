db2756ba45309d9fa95d3e6e08e2e4f730e9d980
Gordon Beeming <gpg@gordonbeeming.com>
2023-01-18 19:12:54 +0200

    remove gh domain from preview publish

--- infrastructure/github-repo.tf
+++ infrastructure/github-repo.tf
@@ -5,9 +5,18 @@ data "github_repository" "main" {
 
 resource "azuread_application_federated_identity_credential" "preview" {
   application_object_id = azuread_application.main.object_id
-  display_name          = "gh-${var.environment_name}-${random_integer.ri.result}-${var.appname}-preview"
-  description           = "Deployments for ${var.appname}"
+  display_name          = "gh-${var.environment_name}-${random_integer.ri.result}-${var.appname}-${github_repository_environment.preview.environment}"
+  description           = "Preview Deployments for ${var.appname}"
   audiences             = ["api://AzureADTokenExchange"]
   issuer                = "https://token.actions.githubusercontent.com"
   subject               = "repo:${data.github_user.current.username}/${data.github_repository.main.name}:environment:${github_repository_environment.preview.environment}"
+}
+
+resource "azuread_application_federated_identity_credential" "live" {
+  application_object_id = azuread_application.main.object_id
+  display_name          = "gh-${var.environment_name}-${random_integer.ri.result}-${var.appname}-${github_repository_environment.live.environment}"
+  description           = "Live Deployments for ${var.appname}"
+  audiences             = ["api://AzureADTokenExchange"]
+  issuer                = "https://token.actions.githubusercontent.com"
+  subject               = "repo:${data.github_user.current.username}/${data.github_repository.main.name}:environment:${github_repository_environment.live.environment}"
 }

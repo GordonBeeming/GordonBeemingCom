a68d1475dda86b0ee1194d63f3c693c9ae77f3a7
Gordon Beeming <gpg@gordonbeeming.com>
2023-05-06 19:31:21 +0200

    use docker for local sql

--- src/GordonBeemingCom.Editor/Program.cs
+++ src/GordonBeemingCom.Editor/Program.cs
@@ -1,21 +1,59 @@
-﻿using GordonBeemingCom.Editor.Areas.Identity;
+﻿using System.Globalization;
+using GordonBeemingCom.Editor.Areas.Identity;
 using GordonBeemingCom.Editor.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging.Debug;
+
+var culture = (CultureInfo)CultureInfo.GetCultureInfo("en-us").Clone();
+var cultureNumberFormat = (NumberFormatInfo)culture.NumberFormat.Clone();
+cultureNumberFormat.CurrencySymbol = "R";
+cultureNumberFormat.CurrencyDecimalSeparator = ".";
+cultureNumberFormat.NumberDecimalSeparator = ".";
+culture.NumberFormat = cultureNumberFormat;
+CultureInfo.DefaultThreadCurrentCulture = culture;
+CultureInfo.DefaultThreadCurrentUICulture = culture;
+
+#if DEBUG
+var debugLoggerFactory = new LoggerFactory(new[] { new DebugLoggerProvider() });
+#endif
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+{
+  options.UseSqlServer(connectionString, sqlServerOptions => sqlServerOptions.CommandTimeout(300));
+#if DEBUG
+  options.UseLoggerFactory(debugLoggerFactory);
+  options.EnableSensitiveDataLogging(true);
+#endif
+});
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+  options.UseSqlServer(connectionString, sqlServerOptions => sqlServerOptions.CommandTimeout(300));
+#if DEBUG
+  options.UseLoggerFactory(debugLoggerFactory);
+  options.EnableSensitiveDataLogging(true);
+#endif
+});
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+  options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+  options.KnownNetworks.Clear();
+  options.KnownProxies.Clear();
+});
+
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
---
+++ src/GordonBeemingCom/Data/AppDbContextFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace GordonBeemingCom.Data;
+
+public sealed class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+  public AppDbContext CreateDbContext(string[] args)
+  {
+    var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+    optionsBuilder.UseSqlServer();
+    return new AppDbContext(optionsBuilder.Options);
+  }
+}

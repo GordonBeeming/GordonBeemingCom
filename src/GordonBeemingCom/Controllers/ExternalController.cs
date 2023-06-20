564fa0acfa320ce9b0d8948506c7207980457f0c
Gordon Beeming <gpg@gordonbeeming.com>
2023-06-20 20:43:25 +0200

    Show hint that a link is going external to the site when it is 🙂

---
+++ src/GordonBeemingCom/Controllers/ExternalController.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using GordonBeemingCom.Areas.Blog.ViewModels;
+using GordonBeemingCom.Data;
+using Microsoft.AspNetCore.OutputCaching;
+using Microsoft.EntityFrameworkCore;
+
+namespace GordonBeemingCom.Controllers;
+
+public sealed class ExternalController : Controller
+{
+  private readonly ILogger<ExternalController> _logger;
+  private readonly AppDbContext _context;
+  private readonly IConfiguration _configuration;
+
+  public ExternalController(ILogger<ExternalController> logger, AppDbContext context, IConfiguration configuration)
+  {
+    _logger = logger;
+    _context = context;
+    _configuration = configuration;
+  }
+
+  [HttpGet("/external")]
+  public IActionResult Index([FromQuery]string link)
+  {
+    _logger.LogInformation("External link clicked {link}", link);
+
+    // later we could track this and potentially handle redirects here instead of updating content
+
+    return Redirect(link);
+  }
+}
--- src/GordonBeemingCom/wwwroot/js/site.js
+++ src/GordonBeemingCom/wwwroot/js/site.js
@@ -2,3 +2,28 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+  // Loop through each anchor tag on the page
+  $('a').each(function () {
+    var a = new RegExp('/' + window.location.host + '/');
+    // Check if it's an external link
+    if (!a.test(this.href)) {
+      // If the link contains only text
+      if ($(this).children().length == 0) {
+        $(this).append(" <i class='fas fa-external-link-alt'></i>");
+      }
+
+      // Store the original link
+      var originalLink = this.href;
+
+      // Encode the original link
+      var encodedLink = encodeURIComponent(originalLink);
+
+      // Change the href to redirect to the local page /external with the encoded original link as a query string
+      this.href = "/external?link=" + encodedLink;
+      this.target = "_blank"
+      this.title = "This is an external link, and will open in a new tab.";
+    }
+  });
+});

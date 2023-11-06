853d962ce45931ed62fa44ded705d4804ece9000
Gordon Beeming <gpg@gordonbeeming.com>
2023-11-07 07:45:12 +1000

    Try solve #121 differently

--- src/GordonBeemingCom/Controllers/ExternalController.cs
+++ src/GordonBeemingCom/Controllers/ExternalController.cs
@@ -11,17 +11,26 @@ public sealed class ExternalController : Controller
   private readonly ILogger<ExternalController> _logger;
   private readonly AppDbContext _context;
   private readonly IConfiguration _configuration;
+  private readonly IHttpContextAccessor _httpContextAccessor;
 
-  public ExternalController(ILogger<ExternalController> logger, AppDbContext context, IConfiguration configuration)
+  public ExternalController(ILogger<ExternalController> logger, AppDbContext context, IConfiguration configuration, IHttpContextAccessor httpContextAccessor)
   {
     _logger = logger;
     _context = context;
     _configuration = configuration;
+    _httpContextAccessor = httpContextAccessor;
   }
 
   [HttpGet("/external")]
   public IActionResult Index([FromQuery]string link)
   {
+    var referrer = _httpContextAccessor.HttpContext?.Request.Headers["Referer"].ToString();
+    var host = _httpContextAccessor.HttpContext?.Request.Host.ToString();
+    var scheme = _httpContextAccessor.HttpContext?.Request.Scheme.ToString();
+    if (referrer?.StartsWith($"{scheme}://{host}/") != true)
+    {
+      return NotFound();
+    }
     _logger.LogInformation("External link clicked {link}", link.Replace(Environment.NewLine, ""));
 
     // later we could track this and potentially handle redirects here instead of updating content

cabce5290d019b5a25db7e04386bc97ed6f2b778
Gordon Beeming <me@gordonbeeming.com>
2024-03-18 21:51:15 +1000

    ✨ Build: Check deploy version (#193)

---
+++ src/GordonBeemingCom/Controllers/DeployController.cs
@@ -0,0 +1,42 @@
+﻿using GordonBeemingCom.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+
+namespace GordonBeemingCom.Controllers;
+
+[ApiController]
+[AllowAnonymous]
+[ApiExplorerSettings(IgnoreApi = true)]
+[Route("api/[controller]")]
+public sealed class DeployController(ApplicationDbContext applicationDbContext,ILogger<DeployController> logger,IConfiguration configuration) : ControllerBase
+{
+    [HttpPost("database")]
+    public Task<IActionResult> RunDatabaseMigration([FromBody]string databaseName)
+    {
+        return databaseName switch
+        {
+            "Application" => MigrateDatabase(applicationDbContext),
+            _ => Task.FromResult<IActionResult>(BadRequest())
+        };
+    }
+
+    [HttpPost("version")]
+    public IActionResult GetVersion()
+    {
+        return Ok(configuration["COMMIT_HASH"]);
+    }
+
+    private async Task<IActionResult> MigrateDatabase(DbContext context)
+    {
+        try
+        {
+            await context.Database.MigrateAsync();
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, $"An error occurred while migrating the database '{context.Database.GetDbConnection().Database}'.");
+            return Conflict();
+        }
+    }
+}

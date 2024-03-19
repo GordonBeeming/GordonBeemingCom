3dd23e733c6d913ebb58f08223bb355853321e71
Gordon Beeming <gpg@gordonbeeming.com>
2024-03-19 10:36:46 +1000

    null check for external link

--- src/GordonBeemingCom/Controllers/ExternalController.cs
+++ src/GordonBeemingCom/Controllers/ExternalController.cs
@@ -24,6 +24,10 @@ public sealed class ExternalController : Controller
   [HttpGet("/external")]
   public IActionResult Index([FromQuery]string link)
   {
+    if (link == null)
+    {
+      return NotFound();
+    }
     // var referrer = _httpContextAccessor.HttpContext?.Request.Headers["Referer"].ToString();
     // var host = _httpContextAccessor.HttpContext?.Request.Host.ToString();
     // var scheme = _httpContextAccessor.HttpContext?.Request.Scheme.ToString();

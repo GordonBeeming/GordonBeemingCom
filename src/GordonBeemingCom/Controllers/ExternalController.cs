d9447ccc717a8d638190c41a54d2ebd2e6f10080
Gordon Beeming <gpg@gordonbeeming.com>
2024-04-22 18:44:19 +1000

    feat: Add MVP contributor ID to external links
    This commit adds functionality to attach an MVP contributor ID to external links that match specific domains. It also includes a helper method, AttachMvpContributorId, which handles the attachment of the ID to the links.
    
    The MVP contributor ID is set to "DT-MVP-5000879" (Gordon Beeming).
    
    The domains that are considered for attaching the MVP contributor ID are:
    - docs.microsoft.com
    - learn.microsoft.com
    - social.technet.microsoft.com
    - azure.microsoft.com
    - techcommunity.microsoft.com
    - social.msdn.microsoft.com
    - devblogs.microsoft.com
    - developer.microsoft.com
    - channel9.msdn.com
    - gallery.technet.microsoft.com
    - cloudblogs.microsoft.com
    - technet.microsoft.com
    - docs.azure.cn
    - www.azure.cn
    - msdn.microsoft.com
    - blogs.msdn.microsoft.com
    - blogs.technet.microsoft.com
    - microsoft.com/handsonlabs
    - csc.docs.microsoft.com
    - code.visualstudio.com
    
    The AttachMvpContributorId method is copied and altered from Jesse Houwing's code.

--- src/GordonBeemingCom/Controllers/ExternalController.cs
+++ src/GordonBeemingCom/Controllers/ExternalController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using System.Text.RegularExpressions;
 using GordonBeemingCom.Areas.Blog.ViewModels;
 using GordonBeemingCom.Data;
 using Microsoft.AspNetCore.OutputCaching;
@@ -42,7 +43,62 @@ public sealed class ExternalController : Controller
     _logger.LogInformation("External link clicked {link}", link.Replace(Environment.NewLine, ""));
 
     // later we could track this and potentially handle redirects here instead of updating content
-
+    link = AttachMvpContributorId(link);
     return Redirect(link);
+  }
+
+  const string ContributionId = "DT-MVP-5000879";
+
+  static readonly string[] _docsAndLearnChampionDomains =  {
+    "docs.microsoft.com",
+    "learn.microsoft.com",
+    "social.technet.microsoft.com",
+    "azure.microsoft.com",
+    "techcommunity.microsoft.com",
+    "social.msdn.microsoft.com",
+    "devblogs.microsoft.com",
+    "developer.microsoft.com",
+    "channel9.msdn.com",
+    "gallery.technet.microsoft.com",
+    "cloudblogs.microsoft.com",
+    "technet.microsoft.com",
+    "docs.azure.cn",
+    "www.azure.cn",
+    "msdn.microsoft.com",
+    "blogs.msdn.microsoft.com",
+    "blogs.technet.microsoft.com",
+    "microsoft.com/handsonlabs",
+
+    // added from https://github.com/mjisaak/skilling-champion-extension/blob/main/src/main.js
+    "csc.docs.microsoft.com",
+    "code.visualstudio.com",
+  };
+
+  /// <summary>
+  /// Copied and altered from Jesse Houwing
+  /// https://gist.github.com/jessehouwing/7dd997447d5267b77842f4f5bd5a5996
+  /// </summary>
+  /// <param name="link"></param>
+  /// <returns></returns>
+  static string AttachMvpContributorId(string link)
+  {
+    try
+    {
+      Uri linkUri = new Uri(link, UriKind.Absolute);
+
+      bool match = _docsAndLearnChampionDomains.Select(domain => new Uri("https://" + domain, UriKind.Absolute)).Any(y => string.Equals(linkUri.Host, y.Host, StringComparison.OrdinalIgnoreCase) && linkUri.AbsolutePath.StartsWith(y.AbsolutePath, StringComparison.OrdinalIgnoreCase));
+
+      if (match)
+      {
+        linkUri = linkUri.Query?.Length > 0 ? new Uri(linkUri.GetLeftPart(UriPartial.Query) + $"&WT.mc_id={ContributionId}{linkUri.Fragment}") : new Uri(linkUri.GetLeftPart(UriPartial.Path) + $"?WT.mc_id={ContributionId}{linkUri.Fragment}");
+        link = linkUri.AbsoluteUri;
+      }
+
+      string pattern = @"^(https?://\S*?)/([a-z]{2}-[a-z]{2})(/.*)?$";
+      string replacement = "$1$3";
+      link = Regex.Replace(link, pattern, replacement);
+    }
+    catch (UriFormatException) { }
+    return link;
   }
 }

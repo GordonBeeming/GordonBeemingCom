7b226a1a49432da541d15997f45b86b96bca4423
Gordon Beeming <gpg@gordonbeeming.com>
2024-06-01 01:32:27 +1000

    Adding new emoji images

---
+++ src/GordonBeemingCom.Editor/RegexHelpers.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.RegularExpressions;
+
+namespace GordonBeemingCom.Editor;
+
+public partial class RegexHelpers
+{
+  [GeneratedRegex("wlEmoticon wlEmoticon-(\\w+)\"[^>]*src=\"([^\\\"]+)", RegexOptions.IgnoreCase)]
+  internal static partial Regex WlEmoticonPattern();
+}

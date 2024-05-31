9cae7ba6ce0e23ab087625162ef7e7a5e00b998a
Gordon Beeming <gpg@gordonbeeming.com>
2024-06-01 03:16:26 +1000

    Removing it all 😐

--- src/GordonBeemingCom.Editor/Program.cs
+++ src/GordonBeemingCom.Editor/Program.cs
@@ -55,13 +55,6 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options =>
   })
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
-builder.Services.AddAntiforgery(o =>
-{
-  o.HeaderName = "XSRF-TOKEN";
-  o.Cookie.HttpOnly = true;
-  o.Cookie.SecurePolicy = CookieSecurePolicy.Always;
-});
-
 builder.Services.Configure<CookiePolicyOptions>(options =>
 {
   options.MinimumSameSitePolicy = SameSiteMode.Strict;
